ff8250a347684cbcd0d605e9a5bce835cb3743c6
Chris Stanton <cstanton0760@yahoo.com>
2017-05-25 13:16:26 -0500

    added html to order view

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 
-DotEnv = require('dotenv-node');
-new DotEnv();
-var express = require('express');
-var app = express();
-var path = require('path');
-var bodyParser = require('body-parser');
-var router = require('./server/routes/routes.js')
+// sourcing dependancies
+  DotEnv = require('dotenv-node');
+  new DotEnv();
+  var express = require('express');
+  var app = express();
+  var path = require('path');
+  var bodyParser = require('body-parser');
+  var nodemailer = require('nodemailer');
 
-app.use('/inboundURLbase',router)
+// sourcing routes
+  var router = require('./server/routes/routes.js')
 
-//Serve back static files
-app.use(express.static(path.join(__dirname, './public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+// Serve back static files
+  app.use(express.static(path.join(__dirname, './public')));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true}));
 
-//Handle index file separately
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '.public/index.html'));
-})
+// Handles index file separately
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '.public/index.html'));
+  })
 
-app.set('port', process.env.PORT || 5000);
-app.listen(app.get('port'), function() {
+// routes
+
+
+
+// port listening
+  app.set('port', process.env.PORT || 5000);
+  app.listen(app.get('port'), function() {
     console.log('Listening on port: ', app.get('port'));
-});
+  });
--- public/scripts/factories/factory.factory.js
+++ public/scripts/factories/factory.factory.js
@@ -2,6 +2,14 @@
   myApp.factory('FactoryFactory',['$http',function($http) {
     console.log('FactoryFactory running');
 
+// sources notify
+  var notyf = new Notyf({
+        delay:2000,
+        alertIcon: 'fa fa-exclamation-circle',
+        confirmIcon: 'fa fa-check-circle'
+    });
+
+
 
 // adds new user to DB from address view
     function addNewUser(newUserAddress) {
