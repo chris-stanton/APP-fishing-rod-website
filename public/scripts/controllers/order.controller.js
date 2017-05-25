a05112d1d520479a9e4785b3a9d800fec46767b8
Chris Stanton <cstanton0760@yahoo.com>
2017-05-24 22:50:56 -0500

    added thml to order view

--- public/scripts/controllers/order.controller.js
+++ public/scripts/controllers/order.controller.js
@@ -5,6 +5,39 @@ myApp.controller('OrderController',['FactoryFactory', function(FactoryFactory) {
   var self = this;
 
   self.message = 'angular sourced "Order"';
+// hard codes values (needs get request to DB)
+  self.iceRodModels = [
+    {
+      id : 1,
+      blankModel : 'Mini Bite',
+      blankLength : '21"',
+      handleLength : '3"'
+    },
+    {
+      id : 2,
+      blankModel : 'El Dente',
+      blankLength : '28"',
+      handleLength : '5"'
+    },
+    {
+      id : 3,
+      blankModel : 'Perch Pounder',
+      blankLength : '32"',
+      handleLength : '5"'
+    },
+    {
+      id : 4,
+      blankModel : 'Gill Seeker',
+      blankLength : '32"',
+      handleLength : '5"'
+    },
+    {
+      id : 4,
+      blankModel : 'Walleye Stinger',
+      blankLength : '32"',
+      handleLength : '5"'
+    }
+  ];
 
 // customer rod order submission to DB
   self.submitNewOrder = function(newOrder) {
