3b997501dbacd5436a9f423fc8867867c62bb096
Chris Stanton <cstanton0760@yahoo.com>
2017-06-01 17:38:56 -0500

    customers can send emails to me directlty using nodemailer

--- public/scripts/controllers/contact.controller.js
+++ public/scripts/controllers/contact.controller.js
@@ -1,11 +1,44 @@
-myApp.controller('ContactController',['FactoryFactory', function(FactoryFactory) {
+myApp.controller('ContactController',['FactoryFactory', '$http', '$location', '$firebaseAuth', function(FactoryFactory, $http, $location, $firebaseAuth) {
 
   console.log('ContactController running');
 
   var self = this;
+  var auth = $firebaseAuth();
+  var firebaseUser = auth.$getAuth();
+  var notyf = new Notyf({
+        delay: 2000,
+        alertIcon: 'fa fa-exclamation-circle',
+        confirmIcon: 'fa fa-check-circle'
+      });
+
+
 
   self.message = 'angular sourced "Contact"';
 
+// button click to send email to reciever
+  self.sendNewMessage = function(newMessage) {
+    var firebaseUser = auth.$getAuth();
+      if (firebaseUser === null) {
+        swal("You must be logged in to send email", "Try Again!", "error");
+      } else {
+        firebase.auth().currentUser.getIdToken().then(function(idToken) {
+          $http({
+            method: 'POST',
+            url: '/contact/newMessage',
+            data: newMessage,
+            headers: {
+                      id_token : idToken
+                      }
+          }).then(function(response){
+            notyf.confirm('Email Sent');
+              $location.path('/home');
+          }).catch(function(error) {
+            swal("Sorry, we could not send your email.", "Try Again!", "error");
+              console.log('error sending email', error);
+          });
+        });//end of firebase.auth()
+      }// end else
+  };// end sendNewMessage()
 
 
 }]);//end of myApp.controller
--- server/routes/contact.js
+++ server/routes/contact.js
@@ -18,9 +18,29 @@ var transporter = nodemailer.createTransport({
 
 
 
-
-
-
+// sends email from contact view
+  router.post('/newMessage', function (req, res) {
+    var newMessage = req.body;
+    var mailOptions = {
+      from: 'Personal Website ' + process.env.ACCOUNT_NAME,
+      to: process.env.ACCOUNT_RECIEVER_EMAIL,
+      subject: 'Customer Message From Website',
+      text: req.decodedToken.name,
+      html: '<h5>' + 'firebaseUserId: ' + req.decodedToken.uid + '</h5>' +
+            '<h5>' + 'Customer Email: ' + newMessage.customerEmail + '</h5>' +
+            '<h5>' + 'Subject: ' + newMessage.subject + '</h5>' +
+            '<h5>' + 'Message: ' + newMessage.message + '</h5>'
+    };// end var mailOptions
+// sents email to reciever
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          return console.log(error);
+            res.sendStatus(500);
+        }
+         console.log('Message %s sent: %s', info.messageId, info.response);
+          res.sendStatus(201);
+      });//end of transporter
+  });// end router.post()
 
 
 
